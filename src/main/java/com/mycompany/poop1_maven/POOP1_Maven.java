c48008dbb979e6c66b80b6e6b9f0a766ec709b18
adolforc47 <111707538+adolforc47@users.noreply.github.com>
2023-10-12 23:45:11 -0600

    Creación de Práctica 2 ejercicio 2

---
+++ src/main/java/com/mycompany/poop1_maven/POOP1_Maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop1_maven;
+
+/**
+ *
+ * @author Hernandez Perez Mariana Daniela
+ */
+public class POOP1_Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde POO, grupo 8, desde Maven");
+    }
+}
